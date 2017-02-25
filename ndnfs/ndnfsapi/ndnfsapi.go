9ed8ae51317ed2fc7f7c21778d559ef3752f5639
Qeas <alexey.khodos@nexenta.com>
2017-02-25 03:02:11 +0300

    add data network

--- ndnfs/ndnfsapi/ndnfsapi.go
+++ ndnfs/ndnfsapi/ndnfsapi.go
@@ -34,6 +34,7 @@ type Config struct {
 	Name        string // ndnfs
 	Nedgehost   string // localhost
 	Nedgeport   int16 // 8080
+	Nedgedata	string
 	Clustername string
 	Tenantname  string
 	Chunksize   int
@@ -235,7 +236,7 @@ func (c *Client) DeleteVolume(name string) (err error) {
 func (c *Client) MountVolume(name string) (mnt string, err error) {
 	log.Debug(DN, "Mounting Volume ", name)
 
-	nfs := fmt.Sprintf("%s:/%s", c.Config.Nedgehost, name)
+	nfs := fmt.Sprintf("%s:/%s", c.Config.Nedgedata, name)
 	mnt = filepath.Join(c.Config.Mountpoint, name)
 	args := []string{"-t", "nfs", nfs, mnt}
 	if out, err := exec.Command("mount", args...).CombinedOutput(); err != nil {
@@ -248,7 +249,7 @@ func (c *Client) MountVolume(name string) (mnt string, err error) {
 
 func (c *Client) UnmountVolume(name string) (err error) {
 	log.Debug(DN, "Unmounting Volume ", name)
-	nfs := fmt.Sprintf("%s:/%s", c.Config.Nedgehost, name)
+	nfs := fmt.Sprintf("%s:/%s", c.Config.Nedgedata, name)
 	if out, err := exec.Command("umount", nfs).CombinedOutput(); err != nil {
 		log.Error("Error running umount command: ", err, "{", string(out), "}")
 	}
