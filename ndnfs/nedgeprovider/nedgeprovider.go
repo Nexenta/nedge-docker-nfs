1206ce158ac6c0709ac6e571ff667a4c51142d80
sabbot <sabbotagge@gmail.com>
2018-04-17 21:01:11 +0300

    fix unserve method

--- ndnfs/nedgeprovider/nedgeprovider.go
+++ ndnfs/nedgeprovider/nedgeprovider.go
@@ -150,7 +150,7 @@ func (nedge *NexentaEdgeProvider) ServeService(service string, cluster string, t
 
 func (nedge *NexentaEdgeProvider) UnserveService(service string, cluster string, tenant string, bucket string) (err error) {
 	path := fmt.Sprintf("service/%s/serve", service)
-	serve := fmt.Sprintf("clusters/%s/tenants/%s/buckets/%s", cluster, tenant, bucket)
+	serve := fmt.Sprintf("%s/%s/%s", cluster, tenant, bucket)
 
 	//request data
 	data := make(map[string]interface{})
